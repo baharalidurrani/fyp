1ca16539f27322476138e9390558a305a2856326
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-11-14 18:43:03 +0500

    dedicated buttons moved to nav

--- Controllers/AssignController.js
+++ Controllers/AssignController.js
@@ -3,6 +3,6 @@ exports.get = (req, res) => {
 }
 
 exports.post = (req, res) => {
+    //(req.body.reviewer) returns a single item when its one item insted of array
     console.log(req.body.reviewer.length);
-
 }
