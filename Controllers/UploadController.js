ea5981eaee4364c0f0b81e2312da148318f7b98e
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-11-15 19:54:59 +0500

    paper model added | upload post added

--- Controllers/UploadController.js
+++ Controllers/UploadController.js
@@ -1,4 +1,5 @@
 const ConferenceModel = require('../Models/Conference');
+const PaperModel = require('../Models/Paper');
 
 exports.get = (req, res) => {
     ConferenceModel.find().then((conferences) => {
@@ -9,5 +10,21 @@ exports.get = (req, res) => {
 }
 
 exports.post = (req, res) => {
+    console.log('new paper uploaded');
     console.log(req.body);
+
+    var paperModel = new PaperModel({
+        _title: req.body.TITLE,
+        _author: req.user._id,
+        _intro: req.body.INTRO,
+        _file: req.body.FILE,
+        _conference: req.body.CONFERENCE
+    });
+    paperModel.save().then(() => {
+        console.log('paper uploaded successfuly');
+        res.redirect('/repo')
+    }).catch((err) => {
+        console.log(err);
+        res.redirect('/upload');
+    });
 }
---
+++ Models/Paper.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PaperSchema = new Schema({
+    _title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    _author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    _intro: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    _file: {
+        type: String,
+        required: true
+    },
+    _conference: {
+        type: Schema.Types.ObjectId,
+        ref: 'Conference',
+        required: true
+    },
+    _status: {
+        type: String,
+    },
+    _reviews: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Review',
+    },
+    _plagirism: {
+        type: String,
+    }
+});
+
+var PaperModel = mongoose.model('Paper', PaperSchema);
+module.exports = PaperModel;
