6d13648a59c020edbdc1b19641a3bc04b8521ef7
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-11-28 23:27:00 +0500

    review controller skeleton added

---
+++ Controllers/ReviewController.js
@@ -0,0 +1,8 @@
+// const ConferenceModel = require('../Models/Conference');
+// const PaperModel = require('../Models/Paper');
+// const UserModel = require('../Models/User');
+// const ReviewModel = require('../Models/Review');
+
+exports.post = (req, res) => {
+    console.log(req.body);
+}
