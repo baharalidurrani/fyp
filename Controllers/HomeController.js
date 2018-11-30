ff0a0259f711117333d00a12b4f02e1235fc7d79
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-11-30 22:48:07 +0500

    home data retrived from db

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -1,4 +1,24 @@
-module.exports=(req,res)=>
-{
+const PaperModel = require('../Models/Paper');
+const ConferenceModel = require('../Models/Conference');
+
+module.exports = (req, res) => {
+    PaperModel.find({
+        _status: 'published'
+    }).then((papers) => {
+        //intercepting data before sending it to the client side
+        for (var i = 0; i < papers.length; i++) {
+            //replacing the whole User object by only its _name
+            papers[i]._author = papers[i]._author._name;
+            papers[i]._reviews = null;
+        }
+        console.log(papers);
+    });
+
+    ConferenceModel.find().then((conferences) => {
+        for (var i = 0; i < conferences.length; i++) {
+            conferences[i]._reviewers = null;
+        }
+        console.log(conferences);
+    });
     res.render('Home');
 }
