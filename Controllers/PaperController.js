df4aee08057167ab6d9c4efbc65455b1e7649efc
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-12-10 02:40:27 +0500

    if err added in delete paper

--- Controllers/PaperController.js
+++ Controllers/PaperController.js
@@ -3,16 +3,19 @@ const PaperModel = require('../Models/Paper');
 exports.get = (req, res) => {
     if (req.query.AS == "delete") {
         if (req.user._loggedAs == "author") {
-            PaperModel.findByIdAndDelete(req.query.PAPER).then((paper) => {
-                console.log(paper + " deleted");
-                res.redirect('/repo');
-            }).catch((err) => {
-                console.log(err);
-                res.redirect('/');
+            PaperModel.findByIdAndDelete(req.query.PAPER, (err, paper) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect('/');
+                } else {
+                    console.log(paper + " deleted");
+                    res.redirect('/repo');
+                }
             });
+        } else {
+            console.log("postman attack blocked");
+            res.redirect('/');
         }
-        console.log("postman attack blocked");
-        res.redirect('/');
     } else {
         PaperModel.findById(req.query.PAPER).then((paper) => {
             console.log(paper);
