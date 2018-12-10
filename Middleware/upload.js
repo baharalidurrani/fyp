b3bfed242403d9d1bd6a91b259071480e90b02da
Bahar Ali Durrani <baharalidurrani@gmail.com>
2018-12-11 00:34:31 +0500

    upload middleware indented

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -2,15 +2,15 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './views/assets/pdf')
+    cb(null, './views/assets/pdf');
   },
   filename: function (req, file, cb) {
-    cb(null, req.user._id + '-' + Date.now() + '.pdf')
+    cb(null, req.user._id + '-' + Date.now() + '.pdf');
   }
-})
+});
 
 var upload = multer({
   storage: storage
-})
+});
 
 module.exports = upload;
